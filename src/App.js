c45440a06f09595538918d34f454b2ef80be286c
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2023-02-01 11:44:28 +0530

    onboarding form and deails form is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import DriverOnboarding from './components/DriverOnboarding';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/add-user" element={<DriverOnboarding />}/>
+      </Routes>
+  </>
   );
 }
 
